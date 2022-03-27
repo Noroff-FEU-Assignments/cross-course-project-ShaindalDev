309f5cd90904a9df082619bb9554021ede8b6048
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-03-27 23:30:21 +0200

    udated some typos

--- js/newslider.js
+++ js/newslider.js
@@ -1,42 +1,42 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
+var activePosition = 1;
+slider(activePosition);
 
 // forward/Back controls
 function plusSlides(n) {
-  SlideShow(slidePosition += n);
+  slider(activePosition += n);
 }
 
 //  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
+function activeSlide(n) {
+  slider(activePosition = n);
 }
 
-function SlideShow(n) {
+function slider(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
+  var dots = document.getElementsByClassName("dots");
+  if (n > slides.length) {activePosition = 1}
+  if (n < 1) {activePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" enable", "");
   }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+  slides[activePosition-1].style.display = "block";
+  dots[activePosition-1].className += " enable";
 } 
-var slidePosition = 0;
-SlideShow();
+var activePosition = 0;
+slider();
 
-function SlideShow() {
+function slider() {
   var i;
   var slides = document.getElementsByClassName("Containers");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slidePosition++;
-  if (slidePosition > slides.length) {slidePosition = 1}
-  slides[slidePosition-1].style.display = "block";
-  // setTimeout(SlideShow, 000); // Change image every 2 seconds
+  activePosition++;
+  if (activePosition > slides.length) {activePosition = 1}
+  slides[activePosition-1].style.display = "block";
+  setTimeout(slider, 2000); // Change image every 2 seconds
 } 

4b727a6b28bf7b0cdbfc154e2af8f179a9d55f0d
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-04-21 00:40:12 +0200

    updated some functions

--- js/cart.js
+++ js/cart.js
@@ -21,7 +21,7 @@ let cartArray = [];
         <span>Only $${product.prices.price},-</span>
         <small>Ex vat.</small>
       </div>
-      <button type="button" class="btn cart-button" data-product="${product.id}">Add to cart</button>
+      <button type="button" class="btn cart-button" data-product="${product.name}">Add to cart</button>
     </div> `;
 });
 // ADD "BUY BUTTON" FUNCTION
@@ -29,9 +29,9 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(function (button) {
   button.onclick = function (event) {
     
-    cartArray.push(event.target.dataset.product);
+    // cartArray.push(event.target.dataset.product);
     const itemToAdd = products.find(
-      (item) => item.id === event.target.dataset.product
+      (item) => item.name === event.target.dataset.product
     );
     cartArray.push(itemToAdd);
     showCart(cartArray);
@@ -44,14 +44,14 @@ function showCart(cartItems) {
   cart.classList.remove("hidden");
   cartList.innerHTML = "";
   let total = 0;
-  cartItems.forEach(function (cartElement) {
-    total += cartElement.price;
+  cartItems.forEach(function (cartElement, index) {
+    total += cartElement.prices.price;
     cartList.innerHTML += `
         <div class="cart-item">
-          <img class="cart-image" src="${cartElement.image}" alt="${cartElement.name}">
+          <img class="cart-image" src="${cartElement.images[0].src}" alt="${cartElement.name}">
           <div class="cart-details">
             <h6>${cartElement.name}</h6>
-            <span>$${cartElement.price},-</span>
+            <span>$${cartElement.prices.price},-</span>
           </div>
         </div>`;
   });
